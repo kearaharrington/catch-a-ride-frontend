7538461db938e6b31ded2279050a6bd69e21bc9a
kearaharrington <keara.harrington@gmail.com>
2022-11-04 13:03:29 -0700

    edit JourneyOrRideReq (still not working)

--- src/components/JourneyOrRideReq.js
+++ src/components/JourneyOrRideReq.js
@@ -1,29 +1,26 @@
 // Imports
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 import JourneyForm from './JourneyForm';
 import RideReqForm from './RideReqForm';
-const { REACT_APP_SERVER_URL } = process.env;
+// const { REACT_APP_SERVER_URL } = process.env;
 
-const JourneyOrRideForm = (props) => {
-    // const [driver, setDriver] = useState(props.user.driver);
-    const [driver, setDriver] = useState();
-    // const [form, setForm] = useState();
-    let form;
+const JourneyFormContainer = (props) => {
+    const [form, setForm] = useState(props.driver === true? <JourneyForm/> : <RideReqForm/>);
+    const [driver, setDriver] = useState(props.driver);
 
+    // useEffect(() => {if (props.driver === false) {
+    //     setForm(<RideReqForm/>)
+    // }}, [])
     const handleChange = (e) => {
         console.log(e.target.value);
         setDriver(e.target.value)
+        // driver = e.target.value;
     }
-
-    // if (driver === true) {
-    //     form = <JourneyForm/>
-    // } else {
-    //     form = <RideReqForm/>
-    // }
-
+    
     return (
         <div>
+            {props.driver}
             <div className='form-group'>
                 <select className="custom-select" onChange={handleChange}>
                     <option value={true}>Driver</option>
@@ -31,7 +28,24 @@ const JourneyOrRideForm = (props) => {
                 </select>
             </div>
             <div>
-                {driver == true? <JourneyForm/> : <RideReqForm/>}
+                {/* {props.driver === true? <JourneyForm/> : <RideReqForm/>} */}
+                {form}
+            </div>
+        </div>
+    )
+}
+
+const JourneyOrRideForm = (props) => {
+    // const [driver, setDriver] = useState(props.user.driver);
+    const [driver, setDriver] = useState(true);
+    // const [form, setForm] = useState();
+    // let form;
+    // let driver = true;
+
+    return (
+        <div>
+            <div>
+                <JourneyFormContainer driver={driver}/>
             </div>
         </div>
     )
