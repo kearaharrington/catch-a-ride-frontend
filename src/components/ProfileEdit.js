f61b24ecde47bf4eff87c9e9a990db3d1dde7867
kearaharrington <keara.harrington@gmail.com>
2022-11-05 15:54:26 -0700

    add route to App.js, edit JourneyDetails to show date, edit Journey Edit to edit date, started ProfileEdit

---
+++ src/components/ProfileEdit.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+// import DisplayPassengers from './DisplayPassengers';
+// import JourneyDetails from './JourneyDetails';
+const { REACT_APP_SERVER_URL } = process.env;
+
+const ProfileEdit = () => {
+    
+}
+
+export default ProfileEdit;
