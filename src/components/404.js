9886a42d838d69fc24518dcbd156b8c22ccdfc49
Descotto <davidescotto@hotmail.com>
2022-11-06 20:50:55 -0800

    add not found 404

---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h1>Page not found</h1>
+         
+        </div>
+    )
+}
+
+export default Notfound;
