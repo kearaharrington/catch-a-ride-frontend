d9e02c247761235d364aa15063f927271ea97aef
Descotto <davidescotto@hotmail.com>
2022-11-08 22:01:52 -0800

    sync

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-import setAuthToken from '../utils/setAuthToken';
+
+import MessageForm from './MessageForm';
 import Messages from './Messages';
+import Vehicles from './Vehicles';
+import axios from 'axios';
+
+import setAuthToken from '../utils/setAuthToken';
 import Images from './Image';
 import ImageContainer from './ImageContainer';
 const { REACT_APP_SERVER_URL } = process.env;
+
 
 
 const Profile = (props) => {
@@ -32,14 +37,18 @@ const Profile = (props) => {
 
  
 
+
    // make a condition that compares exp and current time
    if (currentTime >= expirationTime) {
        handleLogout();
        alert('Session has ended. Please login to continue.');
    }
 
+
+    
+
    const userData = user ?
-   (<div>
+   (<div className='profile-card'>
        <h1>Profile</h1>
        <ImageContainer photoId={profilePic} />
        <Images />
@@ -49,9 +58,15 @@ const Profile = (props) => {
        <p>ID: {id}</p>
        <div>
 
+        { <MessageForm userId={id}/> }
+        {/* { <Messages user={id} /> } */}
+        <Vehicles user={id}/>
+
+
         <Messages arr={reviewsArr} />
         
         
+
 
        </div>
    </div>) : <h2>Loading...</h2>
@@ -64,6 +79,8 @@ const Profile = (props) => {
         );
     };
     
+
+
     return (
         <div className="text-center pt-4">
             {user ? userData : errorDiv()}
