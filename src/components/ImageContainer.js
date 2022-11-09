4896491ecb8dbb74aacea4c1123fef06abb53393
calvickauer <calvickauer@gmail.com>
2022-11-09 14:40:44 -0800

    Import buffer on profilepic.js and imageContainer.js Profile pics currently working

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {Buffer} from 'buffer';
 import { Redirect, useHistory } from 'react-router-dom';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
@@ -10,8 +11,10 @@ const ImageContainer = (props) => {
     console.log('photos ID before', props.photoId)
 useEffect(() => {
     console.log('photos ID', props.photoId)
-axios.get(`${REACT_APP_SERVER_URL}/images/show/${localStorage.getItem('photoId')}`)
+    console.log(localStorage.getItem('photoId'))
+    axios.get(`${REACT_APP_SERVER_URL}/images/show/${localStorage.getItem('photoId')}`)
         .then(res => {
+            // console.log(res.data)
             setImg(Buffer.from(res.data.image, 'base64').toString('base64'));
         }).catch(err => {
             console.log(err);
